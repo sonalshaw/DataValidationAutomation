25140e7525def483dfe069310b4704781ebeba11
Amit Chinara <amitchinara@gmail.com>
2024-06-21 17:47:55 +0530

    Updated the database pool for SERVER(DataBase Connection)

--- SERVER(DataBase Connection)/routes/controller.js
+++ SERVER(DataBase Connection)/routes/controller.js
@@ -1,29 +1,37 @@
 let express = require('express');
 let router = express.Router();
-let db = require('../services/database')
+let db = require('../services/database');
 
 router.post('/startDataBase', (req, res, next) => {
-    let account_option = req.query.account_option;
+    let account_option = parseInt(req.query.account_option, 10);
     let account;
-    if (account_option === 1)
+
+    if (account_option === 1) {
         account = 'link1';
-    else if (account_option === 2)
+    } else if (account_option === 2) {
         account = 'link2';
-    else
+    } else {
         account = 'link3';
+    }
 
     let username = req.body.username;
     let password = req.body.password;
 
-    db.initialize(account, username, password);
+    db.initialize(account, username, password)
+        .then(() => {
+            res.send('Database initialized successfully.');
+        })
+        .catch((error) => {
+            next(error); // Pass the error to the error handling middleware
+        });
 });
 
 router.get('/getDataBase', (req, res, next) => {
-    return res.send('This is the controller!');
+    return res.send('This is the getDataBase controller!');
 });
 
 router.get('/getTable', (req, res, next) => {
-    return res.send('This is the controller!');
+    return res.send('This is the getTable controller!');
 });
 
 module.exports = router;
